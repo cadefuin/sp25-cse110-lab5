f413b80c0a962604bf8e15792f366929ee62e45e
cadefuin <cassandra.adefuin@gmail.com>
2025-05-05 13:24:32 -0700

    Set up based on requirements

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,51 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // ELEMENT REFERENCES
+
+  const hornDropdown = document.getElementById('horn-select');
+  const volumeSlider = document.getElementById('volume');
+  const playButton = document.querySelector('button');
+
+  // EVENT LISTENERS
+
+  // change horn dropdown menu
+  hornDropdown.onchange = function() {
+    // change image
+
+    // change audio file
+  }
+
+  // change volume slider
+  volumeSlider.onchange = function() {
+    let volume; // get volume value
+
+    // change based on volume level
+    if (volume == 0) {
+
+    }
+    else if (volume < 33) {
+
+    }
+    else if (volume < 67) {
+
+    }
+    else {
+
+    }
+  }
+
+  // click "Play Sound" button
+  playButton.onclick = function() {
+    let horn; // get horn value
+    
+    // play sound at specified volume
+
+    // shoot confetti if party horn
+    if (horn == 'party-horn') {
+
+    }
+  }
 }
+
+
