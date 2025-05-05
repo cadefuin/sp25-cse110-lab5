6233eca7f640724402b4aa5a617704e1c79f2599
cadefuin <cassandra.adefuin@gmail.com>
2025-05-05 15:23:43 -0700

    Add element references

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,6 +5,11 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // ELEMENT REFERENCES
 
+  const smileyImage = document.querySelector('image');
+  const textInput = document.getElementById('text-to-speak');
+  const voiceInput = document.getElementById('voice-select');
+  const talkButton = document.querySelector('button');
+
   // populate Select Voice dropdown
 
 
