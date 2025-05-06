35d96485c93a03833da41f17f07c1c062d728e92
cadefuin <cassandra.adefuin@gmail.com>
2025-05-06 15:28:22 -0700

    Implement speech synthesis using docs

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -7,16 +7,48 @@ function init() {
 
   const smileyImage = document.querySelector('image');
   const textInput = document.getElementById('text-to-speak');
-  const voiceInput = document.getElementById('voice-select');
+  const voiceSelect = document.getElementById('voice-select');
   const talkButton = document.querySelector('button');
 
-  // populate Select Voice dropdown
+  
+  // populate "Select Voice" dropdown
+  const synth = window.speechSynthesis;
+  let voices;
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
 
 
   // EVENT LISTENERS
 
-  // change voice using dropdown
-
-
   // click "Press to Talk" button
+  talkButton.onclick = function() {
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterance.voice = voices[i];
+      }
+    }
+
+    synth.speak(utterance);
+  }
 }
